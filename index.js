601459989088729bf6a64d31d03347b6ba96d573
Raghav Gupta <optimusprime0456@gmail.com>
2023-11-21 15:15:39 +0530

    local To Remote repo

---
+++ index.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById('input-box');
+const addTask = document.getElementById('add-task');
+const tasks = document.querySelector('.tasks')
+
+addTask.addEventListener('click', () => {
+    if(inputBox.value === ''){
+        alert('Please enter the task')
+    }else{
+        let li = document.createElement('li')
+        li.innerHTML = inputBox.value
+        tasks.appendChild(li)
+        let span = document.createElement('span')
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
+    }
+    inputBox.value = ''
+    saveData()
+})
+
+tasks.addEventListener('click', (e) => {
+    if(e.target.tagName === 'LI'){
+        e.target.classList .toggle('checked');
+        saveData()
+    }else if(e.target.tagName === 'SPAN' ){
+        e.target.parentElement.remove()
+        saveData()
+    }
+}, false)
+
+function saveData(){
+    localStorage.setItem('data', tasks.innerHTML)
+}
+
+function showData(){
+    tasks.innerHTML = localStorage.getItem('data')
+}
+
+showData()
